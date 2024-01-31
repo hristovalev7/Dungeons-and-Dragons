1eda970acc63a3823d4e9bb94b656702683f560e
Hristo Valev <hristo.valev7@gmail.com>
2024-02-01 00:21:51 +0200

    Test case

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
+#include "Matrix.hpp"
+#include "Map.hpp"
+
 int main()
 {
+    Matrix<char> matrix(7, 7);
+    matrix.fillMatrix('.');
+    Map map(matrix);
+    map.generateMaze();
+    map.print();
     return 0;
 }

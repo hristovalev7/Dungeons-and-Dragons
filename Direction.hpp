7a3ef277820acbead049240476186f28ce5c6745
Hristo Valev <hristo.valev7@gmail.com>
2024-02-01 00:22:52 +0200

    Added a Direction enum

---
+++ Direction.hpp
@@ -0,0 +1,13 @@
+#ifndef DUNGEONS_AND_DRAGONS_DIRECTION_HPP
+#define DUNGEONS_AND_DRAGONS_DIRECTION_HPP
+
+
+enum Direction
+{
+    Up = 1,
+    Down = -1,
+    Left = 2,
+    Right = -2,
+};
+
+#endif //DUNGEONS_AND_DRAGONS_DIRECTION_HPP

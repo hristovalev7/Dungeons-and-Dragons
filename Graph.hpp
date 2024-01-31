1405a8eb08c9685c5bd02f1199504ca82e1f82a8
Hristo Valev <hristo.valev7@gmail.com>
2024-02-01 00:23:13 +0200

    Added a graph class that inherits Matrix<bool>

---
+++ Graph.hpp
@@ -0,0 +1,19 @@
+#ifndef DUNGEONS_AND_DRAGONS_GRAPH_HPP
+#define DUNGEONS_AND_DRAGONS_GRAPH_HPP
+
+#include "Matrix.hpp"
+#include "Direction.hpp"
+
+class Graph : Matrix<bool>
+{
+public:
+
+    Graph(size_t rows, size_t columns);
+
+    bool canGo(unsigned int i, unsigned int j, const Direction& direction, unsigned int columns) const;
+
+    void addEdge(const std::pair<unsigned int, unsigned int>& node1, const std::pair<unsigned int, unsigned int>& node2, unsigned int matrixColumns);
+};
+
+
+#endif //DUNGEONS_AND_DRAGONS_GRAPH_HPP

aa47419ccac25b2180b5d7547e35bbc6e43f22ab
Hristo Valev <hristo.valev7@gmail.com>
2024-03-02 17:36:43 +0200

    Added helper functions

---
+++ functions.cpp
@@ -0,0 +1,35 @@
+#include "functions.hpp"
+
+unsigned int power(unsigned int x, unsigned int y)
+{
+    unsigned int result{1};
+    for (int i{0}; i < y; ++i)
+    {
+        result = x * result;
+    }
+    return result;
+}
+
+unsigned int numberLength(unsigned int number)
+{
+    unsigned int result{};
+    while (number > 9)
+    {
+        ++result;
+        number = number / 10;
+    }
+    return result + 1;
+}
+
+std::string numberToString(unsigned int number)
+{
+    unsigned int length{numberLength(number)};
+    std::string result;
+    unsigned int multiplier{power(10, length - 1)};
+    while (multiplier > 0)
+    {
+        result.push_back((char) ((number / multiplier) % 10 + '0'));
+        multiplier = multiplier / 10;
+    }
+    return result;
+}
---
+++ functions.hpp
@@ -0,0 +1,12 @@
+#ifndef DUNGEONS_AND_DRAGONS_FUNCTIONS_HPP
+#define DUNGEONS_AND_DRAGONS_FUNCTIONS_HPP
+
+#include <string>
+
+unsigned int power(unsigned int x, unsigned int y);
+
+unsigned int numberLength(unsigned int number);
+
+std::string numberToString(unsigned int number);
+
+#endif //DUNGEONS_AND_DRAGONS_FUNCTIONS_HPP

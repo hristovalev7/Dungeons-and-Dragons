1b64618b75bbb73967e1c969356fb81bd92253b4
Hristo Valev <hristo.valev7@gmail.com>
2024-03-09 20:39:15 +0200

    Created an ItemType enum

---
+++ ItemType.hpp
@@ -0,0 +1,11 @@
+#ifndef DUNGEONS_AND_DRAGONS_ITEMTYPE_HPP
+#define DUNGEONS_AND_DRAGONS_ITEMTYPE_HPP
+
+enum ItemType
+{
+    Weapon,
+    Spell,
+    Armor
+};
+
+#endif //DUNGEONS_AND_DRAGONS_ITEMTYPE_HPP

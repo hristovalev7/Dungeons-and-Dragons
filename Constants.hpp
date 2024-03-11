b0c095f72acf9c72acd769e1773766c81264801e
Hristo Valev <hristo.valev7@gmail.com>
2024-03-11 18:34:03 +0200

    Added an array of treasures

--- Constants.hpp
+++ Constants.hpp
@@ -1,6 +1,8 @@
 #ifndef DUNGEONS_AND_DRAGONS_CONSTANTS_HPP
 #define DUNGEONS_AND_DRAGONS_CONSTANTS_HPP
 
+#include "Item.hpp"
+
 // Player
 #define MAX_HEALTH 50
 
@@ -15,5 +17,16 @@
 #define INITIAL_DRAGON_INTELLECT 25
 #define INITIAL_DRAGON_HEALTH 50
 
+// Treasures
+const Item TREASURES[9]{{"Mail armor",         Armor,  5},
+                        {"Flimsy cloth armor", Armor,  1},
+                        {"Plate armor",        Armor,  10},
+                        {"Rusty sword",        Weapon, 1},
+                        {"Giant sword",        Weapon, 25},
+                        {"Daggers",            Weapon, 22},
+                        {"Meteor",             Spell,  30},
+                        {"A magic trick",      Spell,  1},
+                        {"Arcane blast",       Spell,  25}
+};
 
 #endif //DUNGEONS_AND_DRAGONS_CONSTANTS_HPP

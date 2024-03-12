45cd039d1195a7167129ac4dc95a627aff9f3c54
Hristo Valev <hristo.valev7@gmail.com>
2024-03-12 16:06:48 +0200

    Overloaded operator<< for the Class enum

--- Class.hpp
+++ Class.hpp
@@ -1,10 +1,26 @@
 #ifndef DUNGEONS_AND_DRAGONS_CLASS_HPP
 #define DUNGEONS_AND_DRAGONS_CLASS_HPP
 
+#include <iostream>
+
 enum Class
 {
     Mage,
     Warrior,
 };
+
+inline std::ostream& operator<<(std::ostream& os, const Class& c)
+{
+    switch (c)
+    {
+        case Mage:
+            os << "Mage";
+            break;
+        case Warrior:
+            os << "Warrior";
+            break;
+    }
+    return os;
+}
 
 #endif //DUNGEONS_AND_DRAGONS_CLASS_HPP
